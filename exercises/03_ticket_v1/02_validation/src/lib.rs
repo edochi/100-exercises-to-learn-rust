534d892e8e59314b08d4b4a8c60d67a0f3f9ab4b
echio <echio@deloitte.it>
2025-09-25 17:36:38 +0200

    03-02 done

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -1,3 +1,5 @@
+use std::panic;
+
 struct Ticket {
     title: String,
     description: String,
@@ -18,7 +20,28 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        if title.len() <= 0 {
+            panic!("Title cannot be empty")
+        }
+        if description.len() <= 0 {
+            panic!("Description cannot be empty")
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes")
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes")
+        }
+
+        let possible_statuses = vec![
+            String::from("To-Do"),
+            String::from("In Progress"),
+            String::from("Done"),
+        ];
+        if !possible_statuses.contains(&status) {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed")
+        }
+
         Self {
             title,
             description,
