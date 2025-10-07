e53419043c3c87ba9f6dc4fbc1ec04257065d58d
echio <echio@deloitte.it>
2025-10-08 01:07:13 +0200

    03-12 done

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,78 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u64,
+    unit_price: u64,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u64, unit_price: u64) -> Order {
+        Order::check_product_name(&product_name);
+        Order::check_quantity(&quantity);
+        Order::check_unit_price(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    fn check_product_name(product_name: &String) {
+        if product_name.is_empty() {
+            panic!("Product name can't be empty")
+        }
+        if product_name.len() > 300 {
+            panic!("Product name can't be longer than 300")
+        }
+    }
+
+    fn check_quantity(quantity: &u64) {
+        if *quantity <= 0 {
+            panic!("Quantity must be strictly greater than 0")
+        }
+    }
+
+    fn check_unit_price(unit_price: &u64) {
+        if *unit_price <= 0 {
+            panic!("Unit price must be strictly greater than 0")
+        }
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        Order::check_product_name(&new_product_name);
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u64) {
+        Order::check_quantity(&new_quantity);
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u64) {
+        Order::check_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u64 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u64 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u64 {
+        self.quantity() * self.unit_price()
+    }
+
+    // fn check_quantity(quantity: &u64) {
+    //     if quantity < 0u64 {
+    //         panic!("Quantity can't be less than 0")
+    //     }
+    // }
+}
